406a566c215faefc42b75f8754be7ae246bdeed4
atrapp <andrea.trapp@gmx.net>
2014-07-04 17:53:01 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+  states = State.create([    
+    {name: "Alaska", abbreviation: "AK"},
+    {name: "Arizona", abbreviation: "AZ"},
+    {name: "California", abbreviation: "CA"},
+    {name: "New York", abbreviation: "NY"},
+    {name: "Oregon", abbreviation: "OR"}
+  ]) 
+
+  cities = City.create([
+    {name: "Anchorage", population: "301,134", state_id: 1},
+    {name: "Fairbanks", population: "31,535", state_id: 1},
+    {name: "Phoenix", population: "1,513,367", state_id: 2},
+    {name: "Los Angeles", population: "3,904,657", state_id: 3},
+    {name: "San Francisco", population: "837,442", state_id: 3},
+    {name: "Albany", population: "97,904", state_id: 4},    
+    {name: "New York City", population: "8,405,837", state_id: 4},
+    {name: "Portland", population: "609,456", state_id: 5}
+  ])
