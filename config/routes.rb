a904ddc02b3e997488b30aad65da4a1309345bef
atrapp <andrea.trapp@gmx.net>
2014-07-04 16:27:16 -0400

    adds the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,24 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  resources :states do
+    resources :cities, except: [:show, :index]
+  end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
+
+
+#          Prefix Verb   URI Pattern                                 Controller#Action
+#    state_cities POST   /states/:state_id/cities(.:format)          cities#create
+#  new_state_city GET    /states/:state_id/cities/new(.:format)      cities#new
+# edit_state_city GET    /states/:state_id/cities/:id/edit(.:format) cities#edit
+#      state_city PATCH  /states/:state_id/cities/:id(.:format)      cities#update
+#                 PUT    /states/:state_id/cities/:id(.:format)      cities#update
+#                 DELETE /states/:state_id/cities/:id(.:format)      cities#destroy
+#          states GET    /states(.:format)                           states#index
+#                 POST   /states(.:format)                           states#create
+#       new_state GET    /states/new(.:format)                       states#new
+#      edit_state GET    /states/:id/edit(.:format)                  states#edit
+#           state GET    /states/:id(.:format)                       states#show
+#                 PATCH  /states/:id(.:format)                       states#update
+#                 PUT    /states/:id(.:format)                       states#update
+#                 DELETE /states/:id(.:format)                       states#destroy
