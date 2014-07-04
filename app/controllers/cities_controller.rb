8aa8476b1d1d1296e38989dce0e3575ba0a2fb49
atrapp <andrea.trapp@gmx.net>
2014-07-04 17:14:46 -0400

    adds actions to controllers

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,31 @@
 class CitiesController < ApplicationController
+
+  def new
+    @state = State.find( params[:state_id] )
+    # show me a create form
+  end
+
+  def create
+    state = State.find( params[:state_id] )
+    city = City.create( city_params )
+    state.cities << city
+  end
+
+  def edit
+    @state = State.find( params[:state_id] )
+    @city = City.find( params[:id] )   
+    # show me an update form 
+  end
+
+  def update
+    city = City.find( params[:id] )
+    city.update( city_params )
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name, :population, :state_id)
+  end
+
 end
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,43 @@
 class StatesController < ApplicationController
+
+  def index
+    @states = State.all
+  end
+
+  def new
+    # show me a create form!!
+  end
+
+  def create
+    new_state = State.create( state_params )
+    redirect_to "/states/#{new_state.id}"
+  end
+
+  def edit
+    @state = State.find( params[:id] )
+    # show me an update form!!
+  end
+
+  def update
+    edited_state = State.find( params[:id] )
+    edited_state.update( state_params )
+    redirect_to "/states/#{edited_state.id}"
+  end
+
+  def show
+    @state = State.find( params[:id] )
+    # show me the state page
+  end
+
+  def destroy
+    State.delete( params[:id] )
+    redirect_to "/states"
+  end
+
+  private
+
+  def state_params
+    params.require(:state).permit(:name, :abbreviation)
+  end
+
 end
